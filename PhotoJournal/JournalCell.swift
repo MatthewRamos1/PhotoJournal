1a1c4912a729510098868fc5d2de0312cf31faa1
Matthew Ramos <mmramos309@gmail.com>
2020-01-27 00:05:14 -0500

    Began implementation of layout subviews and long press functionality, configuration function for custom cell implemented

--- PhotoJournal/JournalCell.swift
+++ PhotoJournal/JournalCell.swift
@@ -10,4 +10,39 @@ import UIKit
 
 class JournalCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var entryNameLabel: UILabel!
+    @IBOutlet weak var dateNameLabel: UILabel!
+    
+    
+    private lazy var longPressGesture: UILongPressGestureRecognizer =   {
+        let gesture = UILongPressGestureRecognizer()
+        gesture.addTarget(self, action: #selector(longPressAction(gesture:)))
+        return gesture
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = 20.0
+        backgroundColor = .green
+        addGestureRecognizer(longPressGesture)
+    }
+    
+    
+    
+    @objc
+    private func longPressAction(gesture: UILongPressGestureRecognizer) {
+        if gesture.state ==  .began {
+            gesture.state = .cancelled
+            return
+        }
+        
+    }
+    
+    public func configureCell(journalEntry: JournalEntry) {
+        guard let image = UIImage(data: journalEntry.imageData) else {
+            return
+        }
+        imageView.image = image
+    }
 }
