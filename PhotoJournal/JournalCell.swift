935227ad6946e6556e767e356de26dca532efa66
Matthew Ramos <mmramos309@gmail.com>
2020-01-26 23:36:09 -0500

    Created and constrained collection view with toolbar and bar button items

---
+++ PhotoJournal/JournalCell.swift
@@ -0,0 +1,13 @@
+//
+//  JournalCell.swift
+//  PhotoJournal
+//
+//  Created by Matthew Ramos on 1/26/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class JournalCell: UICollectionViewCell {
+    
+}
