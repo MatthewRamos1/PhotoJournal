9969993ff2d6968b9fa80059a293b0120e963bbb
Matthew Ramos <mmramos309@gmail.com>
2020-01-26 23:22:35 -0500

    Initial Commit

---
+++ PhotoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Matthew Ramos on 1/26/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
