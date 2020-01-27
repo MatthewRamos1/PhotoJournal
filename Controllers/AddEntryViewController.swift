b98065337ad1b2cbb7d9c4b98e482fdae4ec5b40
Matthew Ramos <mmramos309@gmail.com>
2020-01-27 16:17:00 -0500

    Added view controller extension for alert, began set up for appending/saving persistence function

--- Controllers/AddEntryViewController.swift
+++ Controllers/AddEntryViewController.swift
@@ -9,32 +9,51 @@
 import UIKit
 
 class AddEntryViewController: UIViewController {
-
+    
     @IBOutlet weak var journalEntryTextView: UITextView!
     @IBOutlet weak var entryImage: UIImageView!
     
     private var imagePickerController = UIImagePickerController()
-    private let alertController = UIAlertController()
-    
+    private var selectedImage: UIImage? {
+        didSet {
+            appendNewPhotoToJournal()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    func showImageController() {
+    private func appendNewPhotoToJournal() {
+        
+    }
+    private func setImageController() {
         imagePickerController.sourceType = .photoLibrary
-        present(imagePickerController, animated: true)
+        
     }
     
     @IBAction func addGalleryImagePressed(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController()
         let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { [weak self] (alertAction) in
-            self?.showImageController()
+            self?.setImageController()
         }
         alertController.addAction(photoLibraryAction)
+        present(imagePickerController, animated: true)
     }
     
     @IBAction func addCameraImagePressed(_ sender: UIBarButtonItem) {
     }
     
     
+}
+
+extension AddEntryViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            showAlert(title: "Error", message: "Image was not found")
+            return
+        }
+        selectedImage = image
+    }
 }
